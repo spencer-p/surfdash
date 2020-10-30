6dd8d5683987121c1eaa5e76cb2d9227663f5061
Spencer Peterson <spencerjpeterson@gmail.com>
2020-10-30 10:12:03 -0700

    serve http

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/spencer-p/surfdash/pkg/meta"
@@ -9,24 +12,50 @@ import (
 	"github.com/spencer-p/surfdash/pkg/sunset"
 )
 
-func main() {
+func fetchGoodTimes(numDays int) ([]meta.GoodTime, error) {
 	query := noaa.PredictionQuery{
 		Start:    time.Now(),
-		Duration: 14 * 24 * time.Hour,
+		Duration: time.Duration(numDays) * 24 * time.Hour,
 		Station:  noaa.SantaCruz,
 	}
 
 	preds, err := noaa.GetPredictions(&query)
 	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
+		return nil, err
 	}
 
 	sunevents := sunset.GetSunEvents(time.Now(), query.Duration, sunset.SantaCruz)
 
 	goodTimes := meta.GoodTimes(meta.Conditions{preds, sunevents})
+	return goodTimes, nil
+}
 
-	for _, gt := range goodTimes {
-		fmt.Printf("%s\n", gt.String())
+func serveGoodTimes(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", r.Method, r.URL)
+	w.Header().Add("Content-Type", "text/plain")
+	goodTimes, err := fetchGoodTimes(7) // 7 days of forecast
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed to get data: %+v", err)
+		log.Printf("Failed to get data: %+v", err)
+		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	for _, gt := range goodTimes {
+		fmt.Fprintf(w, "%s\n", gt.String())
+	}
+}
+
+func main() {
+	PORT := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		PORT = envPort
+	}
+
+	http.HandleFunc("/api/v1/goodtimes", serveGoodTimes)
+
+	addr := "0.0.0.0:" + PORT
+	log.Printf("Listening and serving on %s", addr)
+	log.Println(http.ListenAndServe(addr, nil))
 }
