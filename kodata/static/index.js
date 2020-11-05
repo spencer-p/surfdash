0652d6242b4444906d94c787fc3139c84b3c4111
Spencer Peterson <spencerjpeterson@gmail.com>
2020-11-04 20:15:18 -0800

    clean web dev a bit

--- kodata/static/index.js
+++ kodata/static/index.js
@@ -1,8 +1,15 @@
 var app = new Vue({
 	el: '#goodtimes',
-	data: { goodtimes: [] }
-})
+	data: { goodtimes: [], no_data: "No good times found." }
+});
 
 fetch('api/v1/goodtimes?o=json')
-  .then(response => response.json())
-  .then(data => app.goodtimes = data);
+	.then(response => response.json())
+	.then(data => app.goodtimes = data)
+	.catch((error) => {
+		console.error('Error:', error);
+		app.goodtimes = [];
+	});
+
+document.querySelector('#beforeload').hidden = true;
+document.querySelector('#goodtimes').hidden = false;
