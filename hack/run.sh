f7c83a338e4a9130fac2612577497b3dded1a165
Spencer Peterson <spencerjpeterson@gmail.com>
2020-11-25 18:35:36 -0800

    run.sh sets env intelligently

--- hack/run.sh
+++ hack/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export KO_DATA_PATH="kodata/"
-export PREFIX="/surfdash/"
-export PORT=8080
+export KO_DATA_PATH=${KO_DATA_PATH:-"kodata/"}
+export PREFIX=${PREFIX:-"/surfdash/"}
+export PORT=${PORT:-8080}
 go run main.go
