35d639d98d0c4e21b5655cce6c28f92eefd9250e
Spencer Peterson <spencerjpeterson@gmail.com>
2021-04-04 23:33:15 -0700

    deploy script issue

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -4,6 +4,4 @@ set -o pipefail -o xtrace -o errexit
 
 # Docker requires root on my system. I awkwardly have to propagate config to the
 # root user, but use kubectl configured as my normal user.
-sudo KO_DOCKER_REPO=docker.io/spencerjp $(which ko) resolve -f conf/surfdash-deployment.yaml --platform=linux/arm64 | kubectl apply -f -
-
-kubectl apply -f conf/surfdash-ingress.yaml -f conf/surfdash-service.yaml
+sudo KO_DOCKER_REPO=docker.io/spencerjp $(which ko) resolve -f conf/surfdash.yaml --platform=linux/arm64 | kubectl apply -f -
